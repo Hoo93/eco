37e3d91af9c92581e2e6a1032361d114fff5d77b
Hoo93 <sksk8922@gmail.com>
2024-03-16 19:03:09 +0900

    [ADD] birthyear 에러 메시지 추가

--- src/auth/const/error-message.ts
+++ src/auth/const/error-message.ts
@@ -1,22 +1,16 @@
-export const INVALID_ID_MIN_LENGTH_MESSAGE =
-  '아이디는 최소 6글자 이상이어야 합니다.';
-export const INVALID_ID_MAX_LENGTH_MESSAGE =
-  '아이디는 최대 12글자 이하이어야 합니다.';
+export const INVALID_ID_MIN_LENGTH_MESSAGE = '아이디는 최소 6글자 이상이어야 합니다.';
+export const INVALID_ID_MAX_LENGTH_MESSAGE = '아이디는 최대 12글자 이하이어야 합니다.';
 export const INVALID_ID_MESSAGE = '아이디는 영문,숫자로만 이루어져야 합니다.';
-export const INVALID_PASSWORD_MIN_LENGTH_MESSAGE =
-  '비밀번호는 최소 6글자 이상이어야 합니다.';
-export const INVALID_PASSWORD_MAX_LENGTH_MESSAGE =
-  '비밀번호는 최대 12글자 이하이어야 합니다.';
-export const INVALID_PASSWORD_MESSAGE =
-  '비밀번호는 최소 1개 이상의 영문,숫자, 특수기호 (!@#$%^&*)를 포함해야 합니다';
+export const INVALID_PASSWORD_MIN_LENGTH_MESSAGE = '비밀번호는 최소 6글자 이상이어야 합니다.';
+export const INVALID_PASSWORD_MAX_LENGTH_MESSAGE = '비밀번호는 최대 12글자 이하이어야 합니다.';
+export const INVALID_PASSWORD_MESSAGE = '비밀번호는 최소 1개 이상의 영문,숫자, 특수기호 (!@#$%^&*)를 포함해야 합니다';
 
-export const INVALID_NAME_MIN_LENGTH_MESSAGE =
-  '이름은 최소 2글자 이상이어야 합니다.';
-export const INVALID_NAME_MAX_LENGTH_MESSAGE =
-  '이름은 최대 20글자 이하이어야 합니다.';
+export const INVALID_NAME_MIN_LENGTH_MESSAGE = '이름은 최소 2글자 이상이어야 합니다.';
+export const INVALID_NAME_MAX_LENGTH_MESSAGE = '이름은 최대 20글자 이하이어야 합니다.';
 export const INVALID_NAME_MESSAGE = '이름은 한글,영문로만 이루어져야 합니다';
 
-export const INVALID_MOBILENUMBER_MESSAGE =
-  '핸드폰 번호를 정확히 입력해 주세요.';
+export const INVALID_MOBILENUMBER_MESSAGE = '핸드폰 번호를 정확히 입력해 주세요.';
 export const INVALID_EMAIL_MESSAGE = '이메일 주소를 정확히 입력해 주세요.';
-export const INVALID_BIRTHDAY_MESSAGE = '생년월일 6자리를 입력해 주세요.';
+export const INVALID_BIRTHYEAR_MESSAGE = '생년 4자리를 입력해 주세요.';
+
+export const INVALID_BIRTHDAY_MESSAGE = '생일 4자리를 입력해 주세요.';
