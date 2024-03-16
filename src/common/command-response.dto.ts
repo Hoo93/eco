0a22aa6d97f3107a2e240708a5e899c64d96a115
Hoo93 <sksk8922@gmail.com>
2024-03-16 18:31:52 +0900

    [ADD] CUD용 response dto 구현

---
+++ src/common/command-response.dto.ts
@@ -0,0 +1,13 @@
+export class CommandResponseDto<T> {
+  success: boolean;
+  message: string;
+  data?: T;
+
+  constructor(message: string, data?: T, success: boolean = true) {
+    this.success = success;
+    this.message = message;
+    if (data !== undefined) {
+      this.data = data;
+    }
+  }
+}
