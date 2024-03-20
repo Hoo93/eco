95cf176612435ff04cd170560228cd89cfc793f8
Hoo93 <sksk8922@gmail.com>
2024-03-20 12:56:20 +0900

    [ADD] 회원탈퇴 기능 구현

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateMemberDto } from '../auth/member/dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import { CommandResponseDto } from '../common/response/command-response.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Member } from './entities/member.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MembersService {
+  constructor(@InjectRepository(Member) private memberRepository: Repository<Member>) {}
+
   create(createMemberDto: CreateMemberDto) {
     return 'This action adds a new member';
   }
@@ -21,7 +26,13 @@ export class MembersService {
     return `This action updates a #${id} member`;
   }
 
-  softDelete(id: string, userId: string) {
-    return new CommandResponseDto('SUCCESS DELETE MEMEBER');
+  async softDelete(id: string, userId: string) {
+    if (id !== userId) {
+      throw new BadRequestException('본인의 아이디만 삭제 가능합니다.');
+    }
+
+    await this.memberRepository.softDelete({ id });
+
+    return new CommandResponseDto('SUCCESS DELETE MEMBER');
   }
 }
