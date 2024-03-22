28c53d781ddc02cc159e1685e186e107746aa467
Hoo93 <sksk8922@gmail.com>
2024-03-22 11:15:11 +0900

    [ADD] global prefix 추가

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
+  app.setGlobalPrefix('eco/api')
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
