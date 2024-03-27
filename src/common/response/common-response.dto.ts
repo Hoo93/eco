ea8473222c1021895066ec7bc1f3fc0c3a66e971
Hoo93 <sksk8922@gmail.com>
2024-03-27 13:17:58 +0900

    [DOC] swagger 문서 설명 추가

--- src/common/response/common-response.dto.ts
+++ src/common/response/common-response.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CommonResponseDto<T> {
+  @ApiProperty({ description: '성공여부', type: 'boolean', example: true })
   success: boolean;
+  @ApiProperty({ description: '요청 성공 메시지', type: 'string', example: 'SUCCESS REQUEST' })
   message: string;
+  @ApiPropertyOptional({ description: '데이터', type: 'object', example: {} })
   data?: T;
 
   constructor(message: string, data?: T, success: boolean = true) {
