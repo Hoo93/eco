738c1a55301b1c10de2c5ed53a656e2bf906e6d3
Hoo93 <sksk8922@gmail.com>
2024-03-27 13:19:29 +0900

    [DOC] swagger 문서 설명 추가

--- src/common/response/pageResponse.dto.ts
+++ src/common/response/pageResponse.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PageResponseDto<T> {
+  @ApiProperty({ description: '성공여부', type: 'boolean', example: true })
+  success: boolean;
+  @ApiProperty({ description: '페이지 크기', type: 'number', example: 10 })
   pageSize: number;
+  @ApiProperty({ description: '총 개수', type: 'number', example: 53 })
   count: number;
+  @ApiProperty({ description: '전체 페이지 수', type: 'number', example: 6 })
   totalPage: number;
+  @ApiProperty({ description: '조회 결과', type: 'object' })
   items: T[];
 
-  constructor(pageSize: number, count: number, items: T[]) {
+  constructor(pageSize: number, count: number, items: T[], success = true) {
     this.pageSize = pageSize;
     this.count = count;
     this.totalPage = Math.floor((count + pageSize - 1) / pageSize);
     this.items = items;
+    this.success = success;
   }
 }
--- src/common/response/responseWithoutPagination.dto.ts
+++ src/common/response/responseWithoutPagination.dto.ts
@@ -1,9 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ResponseWithoutPaginationDto<T> {
+  @ApiProperty({ description: '성공여부', type: 'boolean', example: true })
+  success: boolean;
+  @ApiProperty({ description: '총 개수', type: 'number', example: 53 })
   count: number;
+  @ApiProperty({ description: '조회 결과', type: 'object' })
   items: T[];
 
-  constructor(count: number, items: T[]) {
+  constructor(count: number, items: T[], success = true) {
     this.count = count;
     this.items = items;
+    this.success = success;
   }
 }
