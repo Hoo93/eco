57818f6f5e95c81b9a3a3103bc672ebb47e07f1b
Hoo93 <sksk8922@gmail.com>
2024-04-21 02:25:31 +0900

    add: 개발환경 토큰 유효시간 옵션 수정

--- src/auth/const/auth.const.ts
+++ src/auth/const/auth.const.ts
@@ -1,8 +1,10 @@
+import * as process from 'node:process';
+
 export const SALT = 13;
 export const jwtConstants = {
   accessTokenSecret: 'This is my super secret',
   refreshTokenSecret: 'This is my super super secret',
-  accessTokenExpiresIn: '300s',
+  accessTokenExpiresIn: process.env.NODE_ENV === 'production' ? '300s' : '1d',
   refreshTokenExpiresIn: '1d',
   autoLoginRefreshTokenExpiresIn: '30d',
 };
