8fee31a11e039cba89f7dc101544f202e5e5eb4e
Hoo93 <sksk8922@gmail.com>
2024-05-06 18:54:33 +0900

    edit: 삭제 시간, 수정 시간, 생성자 기본 조회 불가능하도록 수정

--- src/common/entities/BaseTimeEntity.ts
+++ src/common/entities/BaseTimeEntity.ts
@@ -1,18 +1,18 @@
 import { Column, CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseTimeEntity {
-  @Column()
+  @Column({ select: false })
   createId: string;
 
-  @CreateDateColumn({ type: 'timestamp' })
+  @CreateDateColumn()
   createdAt: Date;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, select: false })
   updateId: string;
 
-  @UpdateDateColumn({ nullable: true })
+  @UpdateDateColumn({ nullable: true, select: false })
   updatedAt: Date;
 
-  @DeleteDateColumn({ nullable: true })
+  @DeleteDateColumn({ nullable: true, select: false })
   deletedAt: Date;
 }
