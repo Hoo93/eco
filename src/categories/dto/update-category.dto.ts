7aa76528f3ba03650c2dbfaaea2e39e4556951de
Hoo93 <sksk8922@gmail.com>
2024-05-09 14:59:37 +0900

    fix: priority optional 수정

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateCategoryDto } from './create-category.dto';
-import { IsPositive } from 'class-validator';
+import { IsOptional, IsPositive } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
   @IsPositive()
+  @IsOptional()
   @ApiProperty({ description: '카테고리 우선순위', type: 'number' })
   priority: number;
 }
