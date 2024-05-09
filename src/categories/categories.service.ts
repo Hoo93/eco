ec5a5e857f7c85521544bd99cbe61dee98617c66
Hoo93 <sksk8922@gmail.com>
2024-05-09 14:40:36 +0900

    fix: findAll 수정, create 수정

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
-import { DataSource, IsNull, Repository } from 'typeorm';
+import { DataSource, In, IsNull, Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategoryClosure } from './entities/category-closure.entity';
 
 @Injectable()
 export class CategoriesService {
   constructor(
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>,
+    @InjectRepository(CategoryClosure)
+    private categoryClosureRepository: Repository<CategoryClosure>,
     private dataSource: DataSource,
   ) {}
 
@@ -22,16 +25,53 @@ export class CategoriesService {
       if (!ancestor) {
         throw new Error('부모 카테고리가 존재하지 않습니다.');
       }
-      category.ancestor = ancestor;
     }
 
-    const sameDepthCategories = await this.categoryRepository.find({
-      select: { priority: true },
-      where: { ancestorId: createCategoryDto.ancestorId ?? IsNull() },
-      order: { priority: 'DESC' },
-    });
+    const sameDepthCategories = await this.getSameDepthCategories(createCategoryDto.ancestorId);
 
-    category.priority = sameDepthCategories[0].priority + 1;
+    category.priority = sameDepthCategories.length + 1;
+
+    const ancestorClosures = createCategoryDto.ancestorId
+      ? await this.categoryClosureRepository.find({
+          where: { descendantId: createCategoryDto.ancestorId },
+        })
+      : [];
+
+    // Transaction Start
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const manager = queryRunner.manager;
+
+    try {
+      const createdCategory = await manager.save(category);
+
+      const newClosures = ancestorClosures.map((anc) =>
+        this.categoryClosureRepository.create({
+          ancestorId: anc.ancestorId,
+          descendantId: createdCategory.id,
+          depth: anc.depth + 1,
+        }),
+      );
+
+      const selfClosure = this.categoryClosureRepository.create({
+        ancestorId: createdCategory.id,
+        descendantId: createdCategory.id,
+        depth: 0,
+      });
+
+      newClosures.push(selfClosure);
+
+      await manager.save(newClosures);
+
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
 
     return this.categoryRepository.save(category);
   }
@@ -42,30 +82,56 @@ export class CategoriesService {
       throw new Error('존재하지 않는 카테고리입니다.');
     }
 
-    if (updateCategoryDto.ancestorId) {
-      const ancestor = await this.categoryRepository.findOneBy({ id: updateCategoryDto.ancestorId });
-      if (!ancestor) {
-        throw new Error('부모 카테고리가 존재하지 않습니다.');
+    // if (updateCategoryDto.ancestorId) {
+    //   const ancestor = await this.categoryRepository.findOneBy({ id: updateCategoryDto.ancestorId });
+    //   if (!ancestor) {
+    //     throw new Error('부모 카테고리가 존재하지 않습니다.');
+    //   }
+    //   category.ancestor = ancestor;
+    // }
+
+    // 우선순위 검증
+    let sameDepthCategories: Category[] = await this.getSameDepthCategories(updateCategoryDto.ancestorId);
+
+    if (updateCategoryDto.priority) {
+      if (updateCategoryDto.ancestorId) {
+        if (sameDepthCategories.length + 1 < updateCategoryDto.priority) {
+          throw new Error('우선순위가 올바르지 않습니다.');
+        }
+
+        const adjustCategories = sameDepthCategories.filter((c) => c.priority >= updateCategoryDto.priority);
+
+        for (const c of adjustCategories) {
+          c.priority += 1;
+        }
+
+        await this.categoryRepository.save(adjustCategories);
+      } else {
+        // 부모 변경 없이 우선 순위만 변경하는 경우
+        if (sameDepthCategories.length < updateCategoryDto.priority) {
+          throw new Error('우선순위가 올바르지 않습니다.');
+        }
+
+        // 변경 전 보다 변경 후의 우선순위 값이 큰 경우
+        if (updateCategoryDto.priority > category.priority) {
+          const adjustCategories = sameDepthCategories.filter(
+            (c) => c.priority > category.priority && c.priority <= updateCategoryDto.priority,
+          );
+
+          adjustCategories.forEach((c) => (c.priority -= 1));
+
+          await this.categoryRepository.save(adjustCategories);
+        } else {
+          // 변경 전 보다 변경 후의 우선순위 값이 작은 경우
+          const adjustCategories = sameDepthCategories.filter(
+            (c) => c.priority < category.priority && c.priority >= updateCategoryDto.priority,
+          );
+
+          adjustCategories.forEach((c) => (c.priority += 1));
+
+          await this.categoryRepository.save(adjustCategories);
+        }
       }
-      category.ancestor = ancestor;
-    }
-
-    //   // 우선순위 검증
-    let sameDepthCategories: Category[];
-    if (updateCategoryDto.ancestorId) {
-      sameDepthCategories = await this.categoryRepository.find({
-        where: { ancestorId: updateCategoryDto.ancestorId ?? IsNull() },
-        order: { priority: 'DESC' },
-      });
-    } else {
-      // parent가 없는 경우
-      sameDepthCategories = await this.getTopLevelCategories();
-    }
-
-    console.log(sameDepthCategories);
-
-    if (!this.isValidPriority(sameDepthCategories, updateCategoryDto.priority)) {
-      throw new Error('우선순위가 올바르지 않습니다.');
     }
 
     for (const [k, v] of Object.entries(updateCategoryDto)) {
@@ -75,20 +141,37 @@ export class CategoriesService {
     return this.categoryRepository.save(category);
   }
 
+  //
   async findAll() {
-    return this.dataSource.manager.getTreeRepository(Category).findTrees();
+    const cc = await this.categoryClosureRepository.find({
+      where: {
+        depth: In([0, 1]),
+      },
+      relations: {
+        descendant: true,
+      },
+      order: {
+        descendant: {
+          priority: 'ASC',
+        },
+      },
+    });
+
+    const hierarchyCategories = cc.filter((c) => !c.descendant?.ancestorId).map((c) => c.descendant);
+    const depth1 = cc.filter((c) => !!c.descendant?.ancestorId && c.depth === 1);
+
+    for (const category of hierarchyCategories) {
+      category.descendants = this.buildHierarchy(category, depth1);
+    }
+
+    return hierarchyCategories;
   }
 
-  private isValidPriority(categories: Category[], priority: number) {
-    if (categories.map((cat) => cat.priority).includes(priority)) {
-      return false;
-    }
-
-    if (categories.length + 1 < priority) {
-      return false;
-    }
-
-    return true;
+  private async getSameDepthCategories(ancestorId: number | null) {
+    return await this.categoryRepository.find({
+      where: { ancestorId: ancestorId ?? IsNull() },
+      order: { priority: 'ASC' },
+    });
   }
 
   // async update(id: number, updateCategoryDto: UpdateCategoryDto) {
@@ -144,33 +227,15 @@ export class CategoriesService {
   //     await queryRunner.release();
   //   }
   // }
-  //
-  // async findAll() {
-  //   const cc = await this.categoryClosureRepository.find({
-  //     where: { depth: In([0, 1]) },
-  //     relations: {
-  //       descendant: true,
-  //     },
-  //     order: {
-  //       descendant: {
-  //         priority: 'ASC',
-  //       },
-  //     },
-  //   });
-  //
-  //   const depth0 = cc.filter((c) => c.depth === 0);
-  //   const depth1 = cc.filter((c) => c.depth === 1);
-  //
-  //   // depth0, 1을 이용해 최상위 카테고리 조회
-  //   const hierarchyCategories = depth0.filter((c) => !depth1.includes(c)).map((cc) => cc.descendant);
-  //
-  //   for (const category of hierarchyCategories) {
-  //     category.descendants = this.buildHierarchy(category, depth1);
-  //   }
-  //
-  //   return hierarchyCategories;
-  // }
-  //
+
+  private isValidPriority(categories: Category[], priority: number) {
+    if (categories.length + 1 < priority) {
+      return false;
+    }
+
+    return true;
+  }
+
   private getTopLevelCategories() {
     return this.categoryRepository
       .createQueryBuilder('c')
@@ -190,15 +255,16 @@ export class CategoriesService {
   // }
 
   // 재귀적으로 하위 카테고리를 찾아서 반환
-
   // CategoryClosure의 descendant를 조인해줘야 함
-  // private buildHierarchy(root: Category, categoryClosures: CategoryClosure[]) {
-  //   const descendants = categoryClosures.filter((c) => c.ancestorId === root.id).map((c) => c.descendant);
-  //   descendants.forEach((child) => {
-  //     child.descendants = this.buildHierarchy(child, categoryClosures);
-  //   });
-  //   return descendants;
-  // }
+  private buildHierarchy(root: Category, categoryClosures: CategoryClosure[]) {
+    console.log(root);
+    const descendants = categoryClosures.filter((c) => c.ancestorId === root.id).map((c) => c.descendant);
+    descendants.forEach((child) => {
+      child.descendants = this.buildHierarchy(child, categoryClosures);
+    });
+    return descendants;
+  }
+
   //
   // private async copyAncestor(parentId: number, descendantIds: number[]): Promise<CategoryClosure[]> {
   //   const ancestorClosures = await this.categoryClosureRepository.find({
