2d5d13c91d7e5b7fd34f221956822f9a76112a42
Hoo93 <sksk8922@gmail.com>
2024-05-20 12:02:06 +0900

    addˆ: 이미지 프로세서 모듈 추가

--- src/file-manager/file-manager.module.ts
+++ src/file-manager/file-manager.module.ts
@@ -3,9 +3,10 @@ import { FileManagerService } from './file-manager.service';
 import { FileManagerController } from './file-manager.controller';
 import { S3Service } from './s3.service';
 import { ConfigService } from '@nestjs/config';
+import { ImageProcessorService } from './image-processor.service';
 
 @Module({
   controllers: [FileManagerController],
-  providers: [FileManagerService, S3Service, ConfigService],
+  providers: [FileManagerService, S3Service, ConfigService, ImageProcessorService],
 })
 export class FileManagerModule {}
