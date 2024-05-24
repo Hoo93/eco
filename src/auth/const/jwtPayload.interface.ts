27b035de4929e48f945cc4c61e7c7d7ff5340d99
Hoo93 <sksk8922@gmail.com>
2024-05-24 11:13:57 +0900

    edit: JwtPayload 유저타입, 로그인 타입 추가

--- src/auth/const/jwtPayload.interface.ts
+++ src/auth/const/jwtPayload.interface.ts
@@ -1,5 +1,11 @@
+import { UserType } from './user-type.enum';
+import { LoginType } from './login-type.enum';
+import { MemberType } from './member-type.enum';
+
 export interface JwtPayload {
   id: string;
   username: string;
-  userType: string;
+  userType: UserType;
+  loginType?: LoginType;
+  memberType?: MemberType;
 }
