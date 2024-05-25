8f7f62982877ca27af8b2ab63012b5b9503d43ee
Hoo93 <sksk8922@gmail.com>
2024-05-25 09:52:02 +0900

    add: 생성자 함수 추가

--- src/common/response/id-response.dto.ts
+++ src/common/response/id-response.dto.ts
@@ -1,8 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-
 export class IdResponseDto {
-  @ApiProperty({ description: 'PK', type: 'string' })
-  @IsString()
-  id: string;
+  id: any;
+
+  constructor(id: any) {
+    this.id = id;
+  }
 }
